2fe3371227959aedce2c5751e731678e0b283c93
Uni-Dev-Tech <64417125+Uni-Dev-Tech@users.noreply.github.com>
2020-10-19 14:02:23 +0300

    Main files

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Lock : MonoBehaviour
+{
+    // Три положения пина (слева направо)
+    private int firstPinPosition, secondPinPosition, thirdPinPosition;
+
+    [Header("Текстовая визуализация положения пинов:")] [SerializeField]
+    private Text firstPinText;
+    [SerializeField]
+    private Text secondPinText,
+                 thirdPinText;
+
+    [Header("Первый инструмент(отмычка, свойства)")]
+    [SerializeField]
+    private int firstPinInfluenceMasterKey;
+    [SerializeField]
+    private int secondPinInfluenceMasterKey,
+                 thirdPinInfluenceMasterKey;
+
+    [Header("Второй инструмет(щуп, свойства)")]
+    [SerializeField]
+    private int firstPinInfluenceTester;
+    [SerializeField]
+    private int secondPinInfluenceTester, 
+                 thirdPinInfluenceTester;
+
+    [Header("Третий инструмент(нож, свойство)")]
+    [SerializeField]
+    private int firstPinInfluenceKnife;
+    [SerializeField]
+    private int secondPinInfluenceKnife,
+                 thirdPinInfluenceKnife;
+
+    [Header("Окно победы/проигрыша")]
+    [SerializeField]
+    private GameObject winWindow;
+    [SerializeField]
+    private GameObject loseWindow;
+
+    [SerializeField] Timer timerSettings;
+
+    private void Awake()
+    {
+        Initialization();
+    }
+    /// <summary>
+    /// Настройки перед началом игры при запуске
+    /// </summary>
+    private void Initialization()
+    {
+        PinPositionCombination();
+        winWindow.SetActive(false);
+        loseWindow.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    /// <summary>
+    /// Считает комбинацию значения пинов
+    /// </summary>
+    private void PinPositionCombination()
+    {
+        firstPinPosition = 5;
+        secondPinPosition = 5;
+        thirdPinPosition = 5;
+
+        // случайное количество инструментов
+        int instrumentQuantity = Random.Range(1, 4);
+        for (int i = 0; i < instrumentQuantity; i++)
+        {
+            // случайный инструмент 
+            int currentInstrument = Random.Range(0, 3);
+            if (currentInstrument == 0)
+                ChangePinValue(-firstPinInfluenceMasterKey, -secondPinInfluenceMasterKey, -thirdPinInfluenceMasterKey);
+            if (currentInstrument == 1)
+                ChangePinValue(-firstPinInfluenceTester, -secondPinInfluenceTester, -thirdPinInfluenceTester);
+            if (currentInstrument == 2)
+                ChangePinValue(-firstPinInfluenceKnife, -secondPinInfluenceKnife, -thirdPinInfluenceKnife);
+        }
+        ChangePinValueText(firstPinPosition, secondPinPosition, thirdPinPosition);
+
+        // выставляем время и ходы в зависимости от количества инуструментов 
+        timerSettings.timeForLevel = instrumentQuantity * 30f;
+
+        timerSettings.stepsLeft = instrumentQuantity * 3f;
+    }
+
+    /// <summary>
+    /// Изменение значения пинов (исп. отмычки)
+    /// </summary>
+    public void MasterKeyOnClick()
+    {
+        ChangePinValue(firstPinInfluenceMasterKey, secondPinInfluenceMasterKey, thirdPinInfluenceMasterKey);
+        TestForPinValue();
+        ChangePinValueText(firstPinPosition, secondPinPosition, thirdPinPosition);
+        CheckForWin();
+        timerSettings.DecreaseSteps();
+    }
+    /// <summary>
+    /// Изменение значения пинов (исп.щупа)
+    /// </summary>
+    public void TesterOnClick()
+    {
+        ChangePinValue(firstPinInfluenceTester, secondPinInfluenceTester, thirdPinInfluenceTester);
+        TestForPinValue();
+        ChangePinValueText(firstPinPosition, secondPinPosition, thirdPinPosition);
+        CheckForWin();
+        timerSettings.DecreaseSteps();
+    }
+    /// <summary>
+    /// Изменение значения пинов (исп.ножа)
+    /// </summary>
+    public void KnifeOnClick()
+    {
+        ChangePinValue(firstPinInfluenceKnife, secondPinInfluenceKnife, thirdPinInfluenceKnife);
+        TestForPinValue();
+        ChangePinValueText(firstPinPosition, secondPinPosition, thirdPinPosition);
+        CheckForWin();
+        timerSettings.DecreaseSteps();
+    }
+    /// <summary>
+    /// Удерживает значение пинов в диапазоне от 0 до 10
+    /// </summary>
+    private void TestForPinValue()
+    {
+        if (firstPinPosition < 0)
+            firstPinPosition = 0;
+        if (firstPinPosition > 10)
+            firstPinPosition = 10;
+
+        if (secondPinPosition < 0)
+            secondPinPosition = 0;
+        if (secondPinPosition > 10)
+            secondPinPosition = 10;
+
+        if (thirdPinPosition < 0)
+            thirdPinPosition = 0;
+        if (thirdPinPosition > 10)
+            thirdPinPosition = 10;
+    }
+    /// <summary>
+    /// Меняет значение пинов
+    /// </summary>
+    /// <param name="firstPinDifference">Первый пин</param>
+    /// <param name="secondPinDifference">Второй пин</param>
+    /// <param name="thirdPinDifference">Трейтий пин</param>
+    private void ChangePinValue(int firstPinDifference, int secondPinDifference, int thirdPinDifference)
+    {
+        firstPinPosition += firstPinDifference;
+        secondPinPosition += secondPinDifference;
+        thirdPinPosition += thirdPinDifference;
+    }
+    /// <summary>
+    /// Отображает новое значение пинов
+    /// </summary>
+    /// <param name="firstPinNewValue">Первый пин</param>
+    /// <param name="secondPinNewValue">Второй пин</param>
+    /// <param name="thirdPinNewValue">Третий пин</param>
+    private void ChangePinValueText(int firstPinNewValue, int secondPinNewValue, int thirdPinNewValue)
+    {
+        firstPinText.text = firstPinNewValue.ToString();
+        secondPinText.text = secondPinNewValue.ToString();
+        thirdPinText.text = thirdPinNewValue.ToString();
+    }
+    /// <summary>
+    /// Включает окно победы в случаи равенства значений пинов
+    /// </summary>
+    private void CheckForWin()
+    {
+        if (firstPinPosition == secondPinPosition && secondPinPosition == thirdPinPosition)
+        {
+            Time.timeScale = 0f;
+            winWindow.SetActive(true);
+        }
+    }
+    /// <summary>
+    /// Включает окно проигрыша
+    /// </summary>
+    public void EnableLose()
+    {
+        Time.timeScale = 0f;
+        loseWindow.SetActive(true);
+    }
+    /// <summary>
+    /// Повторяет игру
+    /// </summary>
+    public void AgainButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Показывает оставшееся время")]
+    [SerializeField]
+    private Text minutesLeftText;
+    [SerializeField]
+    private Text secondsLeftText;
+
+    [Header("Показывает оставшиеся ходы")]
+    [SerializeField]
+    private Text stepsLeftText;
+
+    [HideInInspector] public float timeForLevel; // время на прохождение (секунды)
+
+    [HideInInspector] public float stepsLeft; // количество ходов
+
+    // Оставшиеся минуты и секунды
+    private float timeMinuteLeft = 0f;
+    private float timeSecondsLeft = 0f;
+
+    // для сохранения уже прошедшего времени Time.time
+    private int timeDifference = 0;
+
+    [SerializeField] private Lock lockSettings;
+    private void Start()
+    {
+        // считаем количество минут и секунд
+        timeMinuteLeft = Mathf.Floor(timeForLevel / 60f);
+        timeSecondsLeft = timeForLevel - (timeMinuteLeft * 60f);
+        timeDifference = Mathf.RoundToInt(Time.time);
+
+        // выводим начальные данные
+        minutesLeftText.text = timeMinuteLeft.ToString();
+        secondsLeftText.text = timeSecondsLeft.ToString();
+        stepsLeftText.text = stepsLeft.ToString();
+    }
+    void Update()
+    {
+        // отсчитываем секунды 
+        secondsLeftText.text = (timeSecondsLeft - (Mathf.Round(Time.time) - timeDifference)).ToString();
+
+        // проверяем остаток ходов
+        if (stepsLeft < 1)
+            lockSettings.EnableLose();
+
+        // в случае обнудения секунд
+        if((timeSecondsLeft - (Mathf.Round(Time.time) - timeDifference)) == 0)
+        {
+            //вычитаем минуту, добавляем 60 секунд, выводим данные и в timeDifference записываем 
+            //время прошедшее с момента начала игры, тем самым "обнуляя" Time.time
+            if(timeMinuteLeft > 0)
+            {
+                timeMinuteLeft--;
+                minutesLeftText.text = timeMinuteLeft.ToString();
+                timeSecondsLeft = 60;
+                secondsLeftText.text = timeSecondsLeft.ToString();
+                timeDifference = Mathf.RoundToInt(Time.time);
+            }
+
+            // если минут и секунд не осталось - проигрыш
+            if(timeMinuteLeft == 0 && (timeSecondsLeft - (Mathf.Round(Time.time) - timeDifference)) == 0)
+            {
+                lockSettings.EnableLose();
+            }
+        }
+    }
+    /// <summary>
+    /// Уменьшает и выводит количество оставшихся ходов
+    /// </summary>
+    public void DecreaseSteps()
+    {
+        stepsLeft--;
+        stepsLeftText.text = stepsLeft.ToString();
+    }
+}
